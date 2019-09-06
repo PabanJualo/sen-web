11d1ab0ee0e2cd9777e36504878620b0f50ccb49
Juan Pablo Oliveira de lima <jejulima43@gmail.com>
2019-09-06 15:21:28 -0300

    Foi realizado requizição assincrona com servidor da API

--- aluno.js
+++ aluno.js
@@ -13,5 +13,10 @@ console.log(botao)
 
 let botaoQGira = Ladda.create(botao[0])
 botaoQGira.start()
+fetch("http://localhost:8080/sen-api/teste.php").then(function(resposta){
+ return resposta.json()
+}).then(function(dados){
+   alert(dados.mensagem)
+})
 })
 }
